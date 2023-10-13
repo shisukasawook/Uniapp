520a03278c0548f1ad7cb257b298b0c1033a2681
shisukasawook <nekima_sa@hotmail.com>
2023-10-13 14:26:48 +1100

    check file exist

--- src/main/java/org/uts/controller/DatabaseController.java
+++ src/main/java/org/uts/controller/DatabaseController.java
@@ -10,8 +10,12 @@ import java.util.List;
 public class DatabaseController {
 
     public static List<Student> readDatabase() throws IOException {
+        File file = new File("student.data");
+        if (!file.isFile()) {
+            file.createNewFile();
+        }
         List<Student> studentList = new ArrayList<>();
-        BufferedReader reader = new BufferedReader(new FileReader("database.data"));
+        BufferedReader reader = new BufferedReader(new FileReader(file));
         for (String line = reader.readLine(); line != null; line = reader.readLine()) {
             ObjectMapper objectMapper = new ObjectMapper();
             studentList.add(objectMapper.readValue(line, Student.class));
@@ -20,14 +24,15 @@ public class DatabaseController {
     }
 
     public static void saveDatabase(List<Student> studentList) throws IOException {
-
-        BufferedWriter writer = new BufferedWriter(new FileWriter("database.data"));
+        File file = new File("student.data");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
         ObjectMapper objectMapper = new ObjectMapper();
-        for(Student student: studentList) {
+        for (Student student : studentList) {
             writer.write(objectMapper.writeValueAsString(student));
             writer.write("\n");
-            
         }
         writer.close();
+
     }
+
 }
