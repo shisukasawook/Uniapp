9e62374fd1867d5d4bcfed2283b7ba5614c7cceb
shisukasawook <nekima_sa@hotmail.com>
2023-10-28 18:20:46 +1100

    split Admin System to function

--- src/main/java/org/uts/Main.java
+++ src/main/java/org/uts/Main.java
@@ -1,25 +1,14 @@
 package org.uts;
 
 import org.uts.controller.DatabaseController;
-import org.uts.controller.MenuController;
+import org.uts.controller.CLIUniApp;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         DatabaseController.initialize();
-        MenuController.displayMenu();
-
-
-//        List<Student> tempList = StudentController.randomStudent();
-//        DatabaseController.saveDatabase(tempList);
-//
-//        List<Student> studentList = DatabaseController.readDatabase();
-//        for(Student student: studentList) {
-//            StudentController.enrolRandomSubject(student);
-//            StudentController.enrolRandomSubject(student);
-//        }
-//
-//        DatabaseController.saveDatabase(studentList);
+        CLIUniApp cliUniApp = new CLIUniApp();
+        cliUniApp.startMenu();
     }
 }
